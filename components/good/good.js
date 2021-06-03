e6fcba2a1cc46e13ee72e55e4aaef62dc72435eb
lxh <lxh@qq.com>
2021-06-03 09:25:48 +0800

    feat:gan

---
+++ components/good/good.js
@@ -0,0 +1,35 @@
+// components/good/good.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    goodsData:{
+      type:Object,
+      value:''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    detailPag(res){
+      let {goods_id} = res.currentTarget.dataset;
+      // console.log('goods_id',goods_id);
+      wx.navigateTo({
+        url: `/pages/goodsDetail/goodsDetail?goods_id=${goods_id}`,
+      })
+    },
+
+   
+
+  }
+})
